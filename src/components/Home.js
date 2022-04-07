db84d9797c9063d66483629ab484efccf72b3c80
Deroy Young <deroy2459@gmail.com>
2022-04-06 22:01:35 -0400

    Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,13 @@
 import React from "react";
+import image from "../shootingstar.jpg";
 
 export default function Home() {
-    return <h1>Home page</h1>
+    return (
+        <main>
+            <img src={image} alt="Shooting Star over lake" className="absolute object-cover w-full h-full" />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg: leading-snug home-name"> Hello, I`m Deroy</h1>
+            </section>
+        </main>
+    )
 }
