5011929770696aa012c60a5269f981b1ee4665ea
Deroy Young <deroy2459@gmail.com>
2022-04-06 21:33:25 -0400

    Nav bar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,41 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default function NavBar() {
     return (
         <header className="bg-red-600">
-            <div>
-                <nav>
-                    <NavLink to="/">
+            <div className="container mx-auto flex justify-between">
+                <nav className="flex">
+                    <NavLink
+                        to="/"
+                        activeClassName="text-white"
+                        className="inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest"
+                    >
                         Home
                     </NavLink>
-                    <NavLink to="/post">
+                    <NavLink
+                        to="/post"
+                        className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+                        activeClassName="text-red-100 bg-red-700"
+                    >
                         Blog Post
                     </NavLink>
-                    <NavLink to="/project">
+                    <NavLink
+                        to="/project"
+                        className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+                        activeClassName="text-red-100 bg-red-700"
+                    >
                         Projects
                     </NavLink>
-                    <NavLink to="/about">
+                    <NavLink
+                        to="/about"
+                        className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+                        activeClassName="text-red-100 bg-red-700"
+                    >
                         About Me
                     </NavLink>
                 </nav>
             </div>
         </header>
-    )
+    );
 }
