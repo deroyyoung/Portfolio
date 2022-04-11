d9c5a74e0720f0e83f79c5b4928f080dfc56f352
deroy young <deroy2459@gmail.com>
2022-04-10 21:21:58 -0400

    updated author image

--- src/components/About.js
+++ src/components/About.js
@@ -33,7 +33,7 @@ export default function About() {
             <div className="p-10 lg:pt-48 container mx-auto relative">
                 <section className="bg-green-800 rounded=lg shadow-2xl lg:flex p-20">
                     <img
-                        src={author.image}
+                        src={author.authorImage}
                         className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"
                         alt={author.name}
                     />
